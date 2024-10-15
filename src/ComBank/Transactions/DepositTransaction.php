c6e920307fba0fecd1867d62c28e718c88f0f794
EricB-59 <151823587+EricB-59@users.noreply.github.com>
2024-10-15 09:55:38 +0200

    begin_of_transactions

--- src/ComBank/Transactions/DepositTransaction.php
+++ src/ComBank/Transactions/DepositTransaction.php
@@ -10,9 +10,15 @@
 use ComBank\Bank\Contracts\BackAccountInterface;
 use ComBank\Transactions\Contracts\BankTransactionInterface;
 
-class DepositTransaction 
+class DepositTransaction implements BankTransactionInterface
 {
+    public function applyTransaction($BankAccountInterface): float{
 
+    }
+    public function getTransactionInfo(): string{
 
-   
+    }
+    public function getAmount(): float{
+
+    }
 }
