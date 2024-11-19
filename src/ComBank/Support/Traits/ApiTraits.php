95c4df102ade98d92d155a3b4a64156f56c4b456
EricB-59 <ebaenac59@gmail.com>
2024-11-19 18:29:55 +0100

    eric

--- src/ComBank/Support/Traits/ApiTraits.php
+++ src/ComBank/Support/Traits/ApiTraits.php
@@ -38,7 +38,28 @@ trait ApiTraits
 
         return $data["result"];
     }
-    //public function detectFraud (BankTransactionInterface $transaction): bool{
+    public function detectFraud (BankTransactionInterface $transaction): bool{
+        $url = "https://673782ba4eb22e24fca55f05.mockapi.io/api/fraud/fraud-api";
 
-    //}
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        $data = json_decode($response, true);
+        curl_close($ch);
+
+        $action = false;
+        $amountTransaction = $transaction->getAmount();
+
+        for ($i=0; $i < count($data); $i++) { 
+            foreach ($data[$i] as $key => $value) {
+                
+            }
+        }
+
+        
+        return $action;
+    }
 }
